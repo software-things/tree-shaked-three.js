74574fed5cbf54b4f8bbfeb844f157c0c4d7d11b
Igor Podlawski <igor.podlawski@gmail.com>
2020-05-22 16:06:08 +0200

    tree shaking

--- src/Three.js
+++ src/Three.js
@@ -157,6 +157,9 @@ export { WebGLUtils } from './renderers/webgl/WebGLUtils.js';
 export * from './constants.js';
 export * from './Three.Legacy.js';
 
+export { GLTFLoader } from './loaders/GLTFLoader.js';
+export { OrbitControls } from './controls/OrbitControls.js';
+
 if ( typeof __THREE_DEVTOOLS__ !== 'undefined' ) {
 
 	/* eslint-disable no-undef */
